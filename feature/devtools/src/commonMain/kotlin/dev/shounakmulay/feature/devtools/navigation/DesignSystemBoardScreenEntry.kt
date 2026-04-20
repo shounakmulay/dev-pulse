3c93b9455944a471bfa95081fc3570eca2ed6588
shounakmulay <shounakmulay@gmail.com>
2026-04-20 14:51:42 +0530

    feat(devtools): pass Navigator into designSystemBoard entry

--- feature/devtools/src/commonMain/kotlin/dev/shounakmulay/feature/devtools/navigation/DesignSystemBoardScreenEntry.kt
+++ feature/devtools/src/commonMain/kotlin/dev/shounakmulay/feature/devtools/navigation/DesignSystemBoardScreenEntry.kt
@@ -2,9 +2,10 @@ package dev.shounakmulay.feature.devtools.navigation
 
 import androidx.navigation3.runtime.EntryProviderScope
 import dev.shounakmulay.core.designsystem.components.devtools.DesignSystemBoard
+import dev.shounakmulay.core.navigation.Navigator
 import dev.shounakmulay.core.navigation.Screen
 
-fun EntryProviderScope<Screen>.designSystemBoard() {
+fun EntryProviderScope<Screen>.designSystemBoard(navigator: Navigator) {
     entry<Screen.DeveloperTools.DesignSystemBoard> {
         DesignSystemBoard()
     }
