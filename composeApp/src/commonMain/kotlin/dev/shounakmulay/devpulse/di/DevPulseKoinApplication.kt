152bdfedf06c44a0214ea094fe159c4d9c247c1e
shounakmulay <shounakmulay@gmail.com>
2026-04-25 17:10:05 +0530

    chore(composeApp): wire NetworkModule into root DI and fix test scope
    Register NetworkModule in DevPulseKoinApplication. Add :core:network
    dependency to composeApp. Move commonTest dependencies inside the
    sourceSets block to fix Gradle sourceSet scoping.

--- composeApp/src/commonMain/kotlin/dev/shounakmulay/devpulse/di/DevPulseKoinApplication.kt
+++ composeApp/src/commonMain/kotlin/dev/shounakmulay/devpulse/di/DevPulseKoinApplication.kt
@@ -1,5 +1,6 @@
 package dev.shounakmulay.devpulse.di
 
+import dev.shounakmulay.core.network.di.NetworkModule
 import dev.shounakmulay.feature.feed.di.FeedModule
 import dev.shounakmulay.feature.home.di.HomeModule
 import dev.shounakmulay.feature.settings.di.SettingsModule
@@ -8,6 +9,7 @@ import org.koin.plugin.module.dsl.koinConfiguration
 
 @KoinApplication(
     modules = [
+        NetworkModule::class,
         HomeModule::class,
         FeedModule::class,
         SettingsModule::class
